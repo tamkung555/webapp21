2de599aa82242d48840d955746d262fc8293f32a
Nopparat Apichatsiritham <nop.ee38@gmail.com>
2019-04-04 11:48:32 +0700

    Edit table result

--- js/search_item.js
+++ js/search_item.js
@@ -16,14 +16,30 @@ fetch('https://api.myjson.com/bins/ux9zk')
         data.forEach((user) => {
             const {result:{material_ID,spec_ID,description}} = user
             var i;
+            let grey_table = '<div class=\"linegreykeyword\">';
+            let white_table = '<div class=\"linewhitekeyword\">';
+            let tablecontainer = '<div class=\"tablelinecontainer\"></div>';
+            let statutencours = '<div class=\"statutencours\">';
+            let statutencours1 = '<div class=\"statutencours1\">';
+            let tablecelcopy = '<div class=\"tablecelcopy\">';
+            let tablecelcopy1 = '<div class=\"tablecelcopy1\">';
+            let background = '<div class=\"background\"></div>';
+            let fond = '<div class=\"fond\"></div>';
+            let lable = '<div class=\"label\">';
+            let div_end = '</div>';
+
             for (i in user.result){
-                html_result += "<tr>" + "<td class=\"statutencours1 lable\">" + user.result[i].spec_ID + "</td>" + 
-                "<td class=\"statutencours lable\">" + user.result[i].material_ID + "</td>" +
-                "<td class=\"tablecelcopy tablecelcopy1 lable\">" + user.result[i].description + "</td>" +
-                "<td class=\"lable\"><a href=\"p11materialid.html\"><div class=\"buttonnext\">" +
-                "<div class=\"rectangle4\"></div><div class=\"next\">NEXT</div></div></a></td></tr>";
+                if (i%2==0){
+                    html_result += grey_table + tablecontainer;
+                }
+                else{
+                    html_result += white_table + tablecontainer;
+                }
+                html_result += statutencours1 + fond + lable + user.result[i].spec_ID + div_end + div_end;
+                html_result += statutencours + fond + lable + user.result[i].material_ID + div_end + div_end;
+                html_result += tablecelcopy + background + tablecelcopy1 + lable + user.result[i].description + div_end + div_end + div_end;
+                html_result += '<a href=\"p11materialid.html\"><div class=\"buttonnext\"><div class=\"rectangle4\"></div><div class=\"next\">NEXT</div></div></a>' + div_end;
             }
-            html_result += "</tbody>";
             document.getElementById('search_result').innerHTML = html_result;
         });
     })
